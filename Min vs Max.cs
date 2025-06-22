f2e1d46a7d31b205fa0ca6e96e861b5be39ec354
REMSHA TARIQ <remshatariq21@gmail.com>
2025-06-22 20:41:10 +0500

    Add files via upload

---
+++ Min vs Max.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 50;
+
+        int maxvalue = Math.Max(Math.Max(a, b), Math.Max(c, d));
+        int minvalue = Math.Min(Math.Min(a, b), Math.Min(c, d));
+
+        Console.WriteLine("Maxvalue: " + maxvalue);
+        Console.WriteLine("Minvalue: " + minvalue);
+    }
+}
