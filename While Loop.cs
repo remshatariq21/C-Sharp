acae80659ea94406ac0c73c1f21735e9a8cb1b71
REMSHA TARIQ <remshatariq21@gmail.com>
2025-06-23 16:44:52 +0500

    Add files via upload

---
+++ While Loop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int count = 1;
+
+        while (count <= 5)
+        {
+            Console.WriteLine("Count is: " + count);
+            count++;
+        }
+
+        Console.WriteLine("Finished counting.");
+    }
+}
