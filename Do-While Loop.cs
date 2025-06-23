642c9a2a50799cdbdc83b3e8c198ab38d8cad5a8
REMSHA TARIQ <remshatariq21@gmail.com>
2025-06-23 16:48:26 +0500

    Add files via upload

---
+++ Do-While Loop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int i = 1;
+
+        do
+        {
+            Console.WriteLine("Number: " + i);
+            i++;
+        }
+        while (i <= 5);
+
+        Console.WriteLine("Loop ended.");
+    }
+}
+
