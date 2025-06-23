c4f292a9cfe3df55730626a88a0f3fda99c56413
REMSHA TARIQ <remshatariq21@gmail.com>
2025-06-23 16:51:56 +0500

    Add files via upload

---
+++ For Each Loop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] fruits = { "Apple", "Banana", "Cherry" };
+
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+    }
+}
