cecb536e3b002d90b040d1c805690f599a3218f4
REMSHA TARIQ <remshatariq21@gmail.com>
2025-06-23 16:40:37 +0500

    Add files via upload

---
+++ For Loop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // For loop to print numbers from 1 to 10
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine("Number: " + i);
+        }
+
+        Console.ReadLine(); // Wait for user input before closing
+    }
+}
+
