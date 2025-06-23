a5661cc15d722cdc817b0dd289248d7b8f437203
REMSHA TARIQ <remshatariq21@gmail.com>
2025-06-23 16:24:51 +0500

    Add files via upload

---
+++ Simple Array.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declare and initialize an array
+        int[] numbers = new int[5] { 10, 20, 30, 40, 50 };
+
+        // Print the array elements
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
